4496be4e758498c8d2532e3fec3da93aad114635
dimaz20022710 <dimaz.20022710@gmail.com>
2020-10-06 16:53:57 +0300

    ball

--- lab3/lab3.3.py
+++ lab3/lab3.3.py
@@ -15,18 +15,42 @@ CYAN = (0, 255, 255)
 BLACK = (0, 0, 0)
 COLORS = [RED, BLUE, YELLOW, GREEN, MAGENTA, CYAN]
 
-def click(event):
-    print(x, y, r)
-    print(event.pos[0], event.pos[1])
 
-'''рисует новый шарик '''
-def new_ball():
+class Ball:
+
+    def __init__(self, cor):
+        self.x = cor[0]
+        self.y = cor[1]
+        self.r = cor[2]
+
+    def draw_ball(self, x, y):
+        self.x = x
+        self.y = y
+        circle(screen, self.color(), (self.x, self.y), self.r)
+
+    def ball_coordinates(self):
+        return (self.x, self.y, self.r)
+
+    def color(self):
+        color = COLORS[randint(0, 5)]
+        return color
+
+
+def coordinates():
+    x = randint(100, 1100)
+    y = randint(100, 900)
+    r = randint(10, 100)
+    return (x, y, r)
+
+'''def new_ball():
+    """рисует новый шарик """
     global x, y, r
     x = randint(100, 1100)
     y = randint(100, 900)
     r = randint(10, 100)
     color = COLORS[randint(0, 5)]
-    circle(screen, color, (x, y), r)
+    circle(screen, color, (x, y), r)'''
+
 
 pygame.display.update()
 clock = pygame.time.Clock()
@@ -35,17 +59,22 @@ finished = False
 success = 0
 fail = 0
 
+a = Ball(coordinates())
+
 while not finished:
     clock.tick(FPS)
+    a.draw_ball(coordinates()[0], coordinates()[1])
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             finished = True
         elif event.type == pygame.MOUSEBUTTONDOWN:
-            if (event.pos[0] - x)**2 + (event.pos[1] - y)**2 < r**2:
+            #a.draw_ball(*event.pos)
+            print(event.pos, a.ball_coordinates())
+            if (event.pos[0] - a.ball_coordinates()[0])**2 + (event.pos[1] - a.ball_coordinates()[1])**2 < a.ball_coordinates()[2]**2:
                 success += 1
             else:
                 fail += 1
-    new_ball()
+    print(a.ball_coordinates())
     pygame.display.update()
     screen.fill(BLACK)
 
